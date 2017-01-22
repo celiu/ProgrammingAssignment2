927f6f270e7209e0c70d8f950373a41f15d3bca5
celiu <Cen@CendeMacBook-Air.local>
2017-01-22 20:54:16 +0100

    Prog.Ass2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## The "makeCacheMatrix" function resets the variable i in current environment to NULL, defines the following 4 functions and returns a named list containing these functions:
+    ## set = store a matrix in the variable x and reset the variable i to     Null in the parent environment
+    ## get = return the x, which is cached in the environment of "makeCacheM    atrix"
+    ## setinverse = store a inverse in the variable i in the parent environment
+    ## getinverse = return the i, which is cached in the environment of "makeCacheMatrix"
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i  <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) {
+        i <<- inverse
+    }
+    getinverse <- function() i
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The "cacheSolve" function returns the cached inverse of a matrix if already calculated, otherwise calculate the inverse of the matrix and cache it in the environment of "makeCacheMatrix"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data)
+    x$setinverse(i)
+    i
 }
